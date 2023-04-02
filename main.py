4ba15fc96f6be5bee551587b8257fddb989086d8
broaderai <74391584+meetttttt@users.noreply.github.com>
2023-04-02 20:24:29 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
-# Hello World
+# Importing the necessary library
 
-print("Hello World")
+import re
+import string
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+from sklearn.metrics import ConfusionMatrixDisplay
+
+# Loading the data
+
+data = pd.read_json(
+    "/kaggle/input/news-headlines-dataset-for-sarcasm-detection/Sarcasm_Headlines_Dataset_v2.json"
+    , lines=True)
+
+# Checking the shape of the data
+
+print(data.shape)
+
+# Displaying the first ten rows of the dataset
+
+print(data.head(10))
+
+# Checking for null values
+
+print(data.isnull().sum())
+
+# Checking for duplicate values
+
+print(data.duplicated().sum())
+
+# Too be continued ...
